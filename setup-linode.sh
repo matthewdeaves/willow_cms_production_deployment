969acd0196f13427af7c925196fd5cecbba693f8
Matthew Deaves <mndeaves@me.com>
2024-11-04 22:46:27 +0000

    Added a deployment script for linode

---
+++ setup-linode.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Function to wipe and deploy
+wipe_and_deploy() {
+    echo "Wiping and deploying the environment..."
+    docker compose build --no-cache && docker compose down -v && docker compose up -d
+}
+
+# Function to deploy
+deploy() {
+    echo "Deploying the environment..."
+    docker compose build --no-cache && docker compose down && docker compose up -d
+}
+
+# Function to restart
+restart() {
+    echo "Restarting the environment..."
+    docker compose down && docker compose up -d
+}
+
+# Ask the user for input
+read -p "Do you want to [W]ipe and deploy, [D]eploy or [R]estart the environment? (w/d/r): " choice
+
+# Check user input and execute corresponding command
+case "${choice,,}" in
+    w)
+        wipe_and_deploy
+        ;;
+    d)
+        deploy
+        ;;
+    r)
+        restart
+        ;;
+    *)
+        echo "Invalid choice. Please choose W for wipe and deploy, D for deploy, or R for restart."
+        ;;
+esac
